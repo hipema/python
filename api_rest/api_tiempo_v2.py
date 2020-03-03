5a5cfe0c22643940ad7c8211b302b5ae2ee34e1b
hipema <a19hipema@iesgrancapitan.org>
2020-03-03 10:00:14 +0100

    Revisiones realizadas en clase

--- api_rest/api_tiempo_v2.py
+++ api_rest/api_tiempo_v2.py
@@ -1,4 +1,7 @@
 """
+Versión 2.0 - Ejercicio corregido en clase.
+IMPORTANTE: no es necesario crear las funciones calculo_media, calculo_max y calculo_min ya que hay funciones que lo permiten directamente.
+
 1. Usando esta API de OpenWeather que nos da el pronóstico del tiempo para una ciudad que se le pide al usuario de los siguientes cinco días, mostrar:
 
 Temperatura media, mínima y máxima (en grados Celsius) para cada día y global.
@@ -6,26 +9,6 @@ Tened en cuenta que las respuestas de esta api referentes a los días y horas us
 """
 
 # Funciones a utilizar en el programa
-
-def calculo_media (temperaturas):
-    sumatorio = 0
-    for i in range (len(temperaturas)):
-        sumatorio += temperaturas[i]
-    return sumatorio / len(temperaturas)
-
-def calculo_max (temperaturas):
-    maximo = -273.15
-    for i in range (len(temperaturas)):
-        if maximo < temperaturas[i]:
-            maximo = temperaturas[i]
-    return maximo
-
-def calculo_min (temperaturas):
-    minimo = 100
-    for i in range (len(temperaturas)):
-        if minimo > temperaturas[i]:
-            minimo = temperaturas[i]
-    return minimo
 
 import requests
 import os
@@ -36,74 +19,39 @@ from datetime import datetime
 url = "http://api.openweathermap.org/data/2.5/forecast"
 # Parámetros
 ciudad = input(f'Introduce la ciudad que deseas buscar: ')
-key = "56e22addb36fca28311139f89294b092"
-#key = os.getenv('$OPENWEATHERID', 'Id no encontrada')
+#key = "56e22addb36fca28311139f89294b092"
+# Para crear la variable de entorno, vamos a "Run/Edit Cofigurations/Enviroment" y ahí añadimos el valor.
+key = os.environ['OPENWEATHERID']
 parametros = {'q':ciudad, 'appid':key, 'units':'metric'}
 
 # petición
 respuesta = requests.get(url, params=parametros)
+if respuesta.status_code != 200:
+    print("Error al hacer la petición", respuesta.status_code)
+    exit(1)
+datos = respuesta.json()
 
-# Variables donde almacenaremos los resultados obtenidos en la api:
-temp = list() # temperatura en cada momento de la medición
-temp_max = list() # temperatura máxima de cada lectura.
-temp_min = list() # temperatura mínima de cada lectura.
+# Cálculos
+dias = dict() # diccionario con clave del día y valor, la lista de medicones del día.
+totales = list() #lista de mediciones de todos los días.
 
-# Variables donde almacenaremos lo cálculos obtenidos en la api:
-temp_media_global = list() # almacenará la temperatura media en cada cambio de día.
-temp_max_global = list() # almacenará los valores máximos de la temperatura de cada día.
-temp_min_global = list() # almacenará los valores minimos de la temperatura de cada día
+for medicion in datos['list']:
+    # fecha y temperatura de la medición
+    dia = medicion['dt_txt'][:10] #devuelve la cadena con los caracteres indicados, truncamos la fecha.
+    temp = float(medicion['main']['temp']) # pasamos a número real, el valor obtenido.
+    # si no tenemos datos de ese día creamos una nueva entrada en el dicionario
+    if not dias.get(dia):
+        dias[dia] = list()
+    # añadimos medición (aquí si existe la clave de este día, bien porque ya exista o bien porque se acabaría de crear)
+    dias[dia].append(temp)
+    totales.append(temp)
 
-if respuesta.status_code == 200:
-    contenido = respuesta.json()    # pasamos contenido a un diccionario
-    dia = datetime.utcfromtimestamp(int(contenido['list'][0]['dt'])).strftime('%d-%m-%Y')
-
-    for valor in contenido['list']:
-        # Comprobamos el día que lee en la iteración para ver si coincide con el día almacenado
-        dia_leido = datetime.utcfromtimestamp(valor['dt']).strftime('%d-%m-%Y')
-
-        if dia_leido == dia:
-            # si coincide con el día almacenado, vamos añadiendo los valores a los arrays correspondientes.
-            temp.append(valor['main']['temp'])
-            temp_max.append((valor['main']['temp_max']))
-            temp_min.append((valor['main']['temp_min']))
-        else:
-            # Si el día no coincide, claculamos los valores a mostrar (temp. media, max y mínima)
-            temp_media_diaria = calculo_media(temp)
-            temp_maxima_diaria = calculo_max(temp_max)
-            temp_minima_diaria = calculo_min(temp_min)
-
-            print(f'El día {dia} se prevén los siguientes valores en {ciudad}: \n'
-                  f'Temperatura media: {temp_media_diaria} \n'
-                  f'Temperatura mínima: {temp_minima_diaria} \n'
-                  f'Temperatura máxima: {temp_maxima_diaria} \n')
-
-            # Almacenamos los valores en los arrays globales
-            temp_media_global.append(temp_media_diaria)
-            temp_min_global.append(temp_minima_diaria)
-            temp_max_global.append(temp_maxima_diaria)
-
-            # Limpiamos las listas diarias, para volver a calcular los datos del nuevo día.
-            temp = list()
-            temp_max = list()
-            temp_min = list()
-
-            # Actualiamos el valor del día en curso
-            dia = dia_leido
-
-            # Añadimos valores del día en curso
-            temp.append(valor['main']['temp'])
-            temp_max.append((valor['main']['temp_max']))
-            temp_min.append((valor['main']['temp_min']))
-
-    # Una vez leídos todos los días, calculamos resultados globales:
-    res_temp_med_global = calculo_media(temp_media_global)
-    res_temp_max_global = calculo_max(temp_max_global)
-    res_temp_min_global = calculo_min(temp_min_global)
-
-    # Mostramos resultados globales:
-    print(f'Estos son los valores obtenidos en ({ciudad}) para los próximos 5 días:\n'
-          f'Temperatura media: {res_temp_med_global}\n'
-          f'Temperatura mínima: {res_temp_min_global}\n'
-          f'Temperatura máxima: {res_temp_max_global}')
-else:
-    print(f'La consulta ha fallado, vuelva a intentarlo introduciendo un valor correcto.')
+# resultados
+print()
+for dia,temps in dias.items(): # recorremos los pares (dia para recorrer las claves, temps para recorrer los valores) items (recorre los pares)
+    #\t --> realiza un tabulador - sum --> calcula el máximo de la lista - min --> calcula el mínimo de la lista
+    print(f'Día {dia[8:]}-{dia[5:7]}-{dia[0:4]}: \tTemperatura media: {(sum(temps)/len(temps)):.2f}º,'
+          f'mínima: {min(temps)}º y máxima {max(temps)}º')
+print()
+print(f'TOTALES:\t\tTemperatura media: {(sum(totales)/len(totales)):.2f}º,'
+      f'mínima: {min(totales)}º y máxima: {max(totales)}')
--- api_rest/api_tiempo_v3.py
+++ api_rest/api_tiempo_v3.py
@@ -1,6 +1,7 @@
 """
-Versión 2.0 - Ejercicio corregido en clase.
+Versión 3.0 - Ejercicio corregido en clase.
 IMPORTANTE: no es necesario crear las funciones calculo_media, calculo_max y calculo_min ya que hay funciones que lo permiten directamente.
+MEJORAS: utilizamos las mediciones de temperatura máxima y mínima que nos presenta el jason devuelto por la API.
 
 1. Usando esta API de OpenWeather que nos da el pronóstico del tiempo para una ciudad que se le pide al usuario de los siguientes cinco días, mostrar:
 
@@ -33,25 +34,31 @@ datos = respuesta.json()
 
 # Cálculos
 dias = dict() # diccionario con clave del día y valor, la lista de medicones del día.
-totales = list() #lista de mediciones de todos los días.
+totales = {'temp':[], 'temp_min':[], 'temp_max':[]} #lista de mediciones de todos los días donde almacenamos los valores obtenidos.
 
 for medicion in datos['list']:
     # fecha y temperatura de la medición
     dia = medicion['dt_txt'][:10] #devuelve la cadena con los caracteres indicados, truncamos la fecha.
     temp = float(medicion['main']['temp']) # pasamos a número real, el valor obtenido.
+    temp_min = float(medicion['main']['temp_min'])  # pasamos a número real, el valor obtenido.
+    temp_max = float(medicion['main']['temp_max'])  # pasamos a número real, el valor obtenido.
     # si no tenemos datos de ese día creamos una nueva entrada en el dicionario
     if not dias.get(dia):
-        dias[dia] = list()
+        dias[dia] = {'temp':[], 'temp_min':[], 'temp_max':[]} # en este caso, tiene un diccionario con tres listas vacías, diferencia respeto v2
     # añadimos medición (aquí si existe la clave de este día, bien porque ya exista o bien porque se acabaría de crear)
-    dias[dia].append(temp)
-    totales.append(temp)
+    dias[dia]['temp'].append(temp)
+    dias[dia]['temp_min'].append(temp_min)
+    dias[dia]['temp_max'].append(temp_max)
+    totales['temp'].append(temp)
+    totales['temp_min'].append(temp_min)
+    totales['temp_max'].append(temp_max)
 
 # resultados
 print()
 for dia,temps in dias.items(): # recorremos los pares (dia para recorrer las claves, temps para recorrer los valores) items (recorre los pares)
     #\t --> realiza un tabulador - sum --> calcula el máximo de la lista - min --> calcula el mínimo de la lista
-    print(f'Día {dia[8:]}-{dia[5:7]}-{dia[0:4]}: \tTemperatura media: {(sum(temps)/len(temps)):.2f}º,'
-          f'mínima: {min(temps)}º y máxima {max(temps)}º')
+    print(f'Día {dia[8:]}-{dia[5:7]}-{dia[0:4]}: \tTemperatura media: {(sum(temps["temp"])/len(temps["temp"])):.2f}º,'
+          f'mínima: {min(temps["temp_min"])}º y máxima {max(temps["temp_max"])}º')
 print()
-print(f'TOTALES:\t\tTemperatura media: {(sum(totales)/len(totales)):.2f}º,'
-      f'mínima: {min(totales)}º y máxima: {max(totales)}')
+print(f'TOTALES:\t\tTemperatura media: {(sum(totales["temp"])/len(totales["temp"])):.2f}º,'
+      f'mínima: {min(totales["temp_min"])}º y máxima: {max(totales["temp_max"])}')
